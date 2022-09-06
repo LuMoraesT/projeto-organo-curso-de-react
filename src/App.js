efd5b03b6255a81a3ade4a2a0f900a9fccd95e28
Luciana Moraes <lucianamoraestx@gmail.com>
2022-09-05 22:14:21 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import Banner from './componentes/Banner/Banner.js';
+import Formulario from './componentes/Formulario/index.js';
+import { useState } from 'react';
+import Time from './componentes/Time/index.js';
+
+function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57c278',
+      corSecundaria: '#d9f7e9',
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria: '#82cffa',
+      corSecundaria: '#e8f8ff',
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria: '#a6d157',
+      corSecundaria: '',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#e06b69',
+      corSecundaria: '#fde7e8',
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#db6ebf',
+      corSecundaria: '#fae9f5',
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#ffba05',
+      corSecundaria: '#fff5d9',
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#ff8a29',
+      corSecundaria: '#ffeede',
+
+    }
+  ]
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    debugger
+    setColaboradores([...colaboradores, colaborador])
+  }
+
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome}
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />)}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,69 @@
+import './Formulario.css'
+import CampoTexto from './../CampoTexto/index';
+import ListaSuspensa from '../ListaSuspensa';
+import Botao from '../Botao';
+import { useState } from 'react'
+
+const Formulario = (props) => {
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, settime] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time 
+        })
+        setNome('') //limpar formulário após cadastrar card
+        setCargo('')
+        setImagem('')
+        settime('')
+    }
+
+    return (
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Nome" 
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+                <CampoTexto 
+                    obrigatorio={true} 
+                    label="Cargo" 
+                    placeholder="Digite seu cargo"
+                    valor={cargo}
+                    aoAlterado={valor => setCargo(valor)}
+                />
+                <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+                    <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Time" 
+                    itens ={props.times}
+                    valor={time}
+                    aoAlterado={valor => settime(valor)}
+                />
+
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+
+
+export default Formulario
